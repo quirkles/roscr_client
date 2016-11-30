618bf144a977d998f8ac2a17edcb3f8a9d28847b
quirkles <al.quirk@gmail.com>
2016-11-29 21:56:25 -0500

    Init session user id reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,20 @@
 import { combineReducers } from 'redux';
 
 import {routerReducer} from 'react-router-redux';
+
 import circles from './circles_reducer';
 import new_circle from './new_circle_reducer';
 import users from './users_reducer';
 import tooltips from './tooltips_reducer';
+import session_user_id from './session_user_id_reducer';
 
 const rootReducer = combineReducers({
   routing: routerReducer,
   users,
   circles,
   new_circle,
-  tooltips
+  tooltips,
+  session_user_id
 });
 
 export default rootReducer;
---
+++ src/reducers/session_user_id_reducer.js
@@ -0,0 +1,11 @@
+const default_state = '1';
+
+const session_user_id_reducer = (state = default_state, action) => {
+    switch (action.type) {
+    //case SET_SESSION_USER_ID:
+    default:
+      return state;
+  }
+};
+
+export default session_user_id_reducer;
