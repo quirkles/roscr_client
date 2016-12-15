59d72b78c4e309cc1ddcfd52471762be38fdda28
quirkles <al.quirk@gmail.com>
2016-12-14 21:51:28 -0500

    Hook up sign up

--- src/utils/requests/auth.js
+++ src/utils/requests/auth.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
 export const do_request_sign_up = ({email_address, password}) =>
-  axios.post('http://localhost:5000/login', {email_address, password});
+  axios.post('http://localhost:5000/signup', {email_address, password});
