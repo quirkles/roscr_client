80a950fc0500d82d249190f3758d3918b57ba0cc
quirkles <al.quirk@gmail.com>
2016-12-25 22:32:55 -0500

    more heroku debugging

--- src/utils/requests/constants.js
+++ src/utils/requests/constants.js
@@ -1,3 +1,1 @@
-export const BASE_URL = process && process.env && process.env.ROSCR_API_ENDPOINT ?
-  process.env.ROSCR_API_ENDPOINT :
-  'http://localhost:5000';
+export const BASE_URL = 'http://localhost:5000';
