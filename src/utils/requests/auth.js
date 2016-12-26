a8be64fcf9c310809148ad0ecd9e306be1d1c10c
quirkles <al.quirk@gmail.com>
2016-12-25 21:53:24 -0500

    Use dynamic base url for auth helpers

--- src/utils/requests/auth.js
+++ src/utils/requests/auth.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+import {BASE_URL} from './constants';
+
 export const do_request_sign_up = ({email_address, password}) =>
-  axios.post('http://localhost:5000/signup', {email_address, password});
+  axios.post(`${BASE_URL}/signup`, {email_address, password});
 
 export const do_request_log_in = ({email_address, password}) =>
-  axios.post('http://localhost:5000/login', {email_address, password});
+  axios.post(`${BASE_URL}/login`, {email_address, password});
