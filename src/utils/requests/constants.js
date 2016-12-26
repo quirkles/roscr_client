668c7f9c4ce4f64aa543a50caa31b68e9c4dfe85
quirkles <al.quirk@gmail.com>
2016-12-25 22:51:07 -0500

    more heroku debugging

--- src/utils/requests/constants.js
+++ src/utils/requests/constants.js
@@ -1,3 +1,5 @@
-let BUILD_TIME_BASE_URL = process.env.ROSCR_API_ENDPOINT || 'http://localhost:5000';
+let BUILD_TIME_BASE_URL = null;
+
+BUILD_TIME_BASE_URL = process.env.ROSCR_API_ENDPOINT ? process.env.ROSCR_API_ENDPOINT : 'http://localhost:5000';
 
 export const BASE_URL = BUILD_TIME_BASE_URL;
