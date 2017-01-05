7952c7bcce33bfe484c84b5f0becabf7b579b559
Alexander Quirk <alexanderquirk@alex-quirk-macbook.local>
2017-01-05 13:08:29 -0500

    Add in some email validation stuffs

--- src/components/modal/body_templates/add_user_template.jsx
+++ src/components/modal/body_templates/add_user_template.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
 
+import {is_required, is_valid_email} from '../../../utils/validators'
+
+const get_email_validation_errors = email_address =>
+  [is_required, is_valid_email].map(validator => validator(email_address)).filter(r => !!r);
+
+
 const add_user_template = ({
   invitee_email_address,
   update_add_user_email_action,
   submit_add_user_action
-}) =>
-<div className='add-user-template'>
-  <h2 className='margin-bottom-one'>Invite a user to ROSCr!</h2>
-  <p className='text-md margin-bottom-one'>Just enter their email below and hit Invite. We'll let you know when they join the community.</p>
-  <input
-    value={invitee_email_address}
-    onChange={update_add_user_email_action}
-    placeholder='person@example.com'
-    className='margin-bottom-one input-field'
-    type='text'
-  />
-  <div>
-    <div
-      className="btn btn-lg primary"
-      onClick={() => submit_add_user_action(invitee_email_address)}
-    >Invite</div>
-  </div>
-</div>;
+}) => {
+  const validation_errors = get_email_validation_errors(invitee_email_address)
+  const process_submit_click = () => {
+    if(!validation_errors.length) {
+      submit_add_user_action(invitee_email_address)
+    }
+  }
+  return(
+    <div className='add-user-template'>
+      <h2 className='margin-bottom-one'>Invite a user to ROSCr!</h2>
+      <p className='text-md margin-bottom-one'>Just enter their email below and hit Invite. We'll let you know when they join the community.</p>
+      <input
+        value={invitee_email_address}
+        onChange={update_add_user_email_action}
+        placeholder='person@example.com'
+        className='margin-bottom-one input-field'
+        type='text'
+      />
+      <ul className='form-errors-list'>
+        {validation_errors.map((error, i) =><li key={i}>{error}</li>)}
+      </ul>
+      <div>
+        <div
+          className="btn btn-lg primary"
+          onClick={process_submit_click}
+        >Invite</div>
+      </div>
+    </div>
+  );
+}
 
 export default add_user_template;
