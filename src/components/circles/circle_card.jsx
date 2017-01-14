eecf57330d8ed0810bfab3faf8b911f84ca42d02
quirkles <al.quirk@gmail.com>
2017-01-14 12:09:12 -0500

    Card tweaks

--- src/components/circles/circle_card.jsx
+++ src/components/circles/circle_card.jsx
@@ -6,14 +6,13 @@ import {Link} from 'react-router';
 import {get_random_element_from_array} from '../../utils/array';
 import {capitalize} from '../../utils/string';
 
-const header_colors = ['dark', 'danger', 'deep-purple', 'blue-grey', 'primary', 'warning', 'danger'];
-const body_colors = ['indigo', 'blue', 'purple']
+const header_colors = ['danger', 'deep-purple', 'blue-grey', 'primary', 'warning', 'danger'];
 
 const circle_component = ({circle}) =>
   <div
     className='col-sm-6 col-md-4 circle-card'
   >
-    <div className='box indigo'>
+    <div className='box'>
       <div className={classnames('box-header', get_random_element_from_array(header_colors))}>
         <div className='padding-half'>
             <h4 className='bold'>
@@ -25,22 +24,30 @@ const circle_component = ({circle}) =>
             </h4>
         </div>
     </div>
-    <div className={classnames('box-body', get_random_element_from_array(body_colors))}>
+    <div className='box-body grey-200'>
         <div className="text-lg">
           <div className="row padding-bottom-one">
             <div className="col-sm-6">
               <i className="fa fa-dollar padding-right-quarter"></i>
               {circle.get('withdrawal_amount')}
+              <small className='text-md text-muted'>: Payout per cycle</small>
             </div>
             <div className="col-sm-6">
               <i className="fa fa-user padding-right-quarter"></i>
               {circle.get('members').size}/{circle.get('participant_count')}
-            </div>
+              <small className='text-md text-muted'>: Members</small>
+              </div>
           </div>
           <div className="row">
             <div className="col-sm-6">
               <i className="fa fa-calendar padding-right-quarter"></i>
               {format('MMM DD', new Date(circle.get('created')))}
+              <small className='text-md text-muted'>: Start Date</small>
+            </div>
+            <div className="col-sm-6">
+              <i className="fa fa-refresh padding-right-quarter"></i>
+              {capitalize(circle.get('cycle_period'))}
+              <small className='text-md text-muted'>: Payouts</small>
             </div>
           </div>
         </div>
