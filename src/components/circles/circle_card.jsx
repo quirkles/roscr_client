8755cd36e23dff21859a9141ef705bf889214881
quirkles <al.quirk@gmail.com>
2017-02-04 19:31:26 -0500

    Standardize the circle and user cards a bit

--- src/components/circles/circle_card.jsx
+++ src/components/circles/circle_card.jsx
@@ -8,9 +8,10 @@ import {get_trust_score_font_class, get_avatar_url} from '../../utils/user_immut
 import {capitalize} from '../../utils/string';
 import FetchingCircle from './fetching_circle';
 
-const header_colors = ['success', 'danger', 'info', 'primary', 'warning'];
+const colors = ['success', 'danger', 'info', 'primary', 'warning'];
 
-const get_circle_color = circle => header_colors[circle.get('circle_name').length * circle.get('participant_count') % header_colors.length]
+const get_circle_color = circle =>
+  colors[(circle.get('circle_name').length + parseInt(circle.get('participant_count'), 10)) % colors.length];
 
 const circle_component = ({circle}) =>
   circle.get('needs_to_be_fetched') ?
@@ -18,68 +19,58 @@ const circle_component = ({circle}) =>
     <div
       className='col-xl-4 col-md-6 col-sm-12 circle-card'
     >
-      <div className='box'>
-        <div className={classnames('box-header', get_circle_color(circle))}>
-          <div className='padding-half'>
-              <h4 className='bold serif'>
-                  <Link
-                      to={`circles/${circle.get('id')}`}
-                  >
-                      {capitalize(circle.get('circle_name'))}
+      <div className='box padding-two text-md'>
+      <div className={classnames('header-container', get_circle_color(circle))}></div>
+        <div className="row padding-bottom-one">
+          <div className="col col-sm-12 serif">
+            {circle.getIn(['created_by', 'needs_to_be_fetched']) ?
+              <div className="italic text-muted">
+                  <i className={classnames('fa fa-spin fa-spinner margin-right-half', `text-${get_circle_color(circle)}`)}/>
+                  Fetching circle creator information...
+              </div> :
+              <div>
+                <div className={`text-center padding-bottom-half b-b bold text-${get_circle_color(circle)}`}>
+                  <Link to={`circles/${circle.get('id')}`}>
+                    {capitalize(circle.get('circle_name'))}
                   </Link>
-              </h4>
-          </div>
-      </div>
-      <div className='box-body'>
-          <div className="text-md">
-            <div className="row padding-bottom-one">
-              <div className="col col-sm-12 serif">
-                {circle.getIn(['created_by', 'needs_to_be_fetched']) ?
-                  <div className="italic text-muted">
-                      <i className={classnames('fa fa-spin fa-spinner margin-right-half', `text-${get_circle_color(circle)}`)}/>
-                      Fetching circle creator information...
-                  </div> :
-                  <div>
-                    <div className="text-center padding-bottom-half b-b">Created By</div>
-                    <div className="text-center margin-top-quarter margin-bottom-quarter">
-                      <span className='margin-right-two'>
-                        <img src={get_avatar_url(circle.get('created_by'))} alt="." className='img-circle w-24'/>
-                      </span>
-                      <Link to={`/users/${circle.getIn(['created_by', 'id'])}`}>
-                        {`${circle.getIn(['created_by', 'firstname'])} ${circle.getIn(['created_by', 'lastname'])}`}
-                      </Link>
-                      <span className={classnames('label margin-left-two', get_trust_score_font_class(circle.getIn(['created_by', 'trust_score'])))}>
-                        {circle.getIn(['created_by', 'trust_score'])}
-                      </span>
-                    </div>
-                  </div>
-                }
-              </div>
-            </div>
-            <div className="row padding-bottom-one">
-              <div className="col-sm-6">
-                <i className="fa fa-dollar padding-right-quarter"></i>
-                {circle.get('withdrawal_amount')}
-                <small className='text-sm text-muted'>: Payout/cycle</small>
-              </div>
-              <div className="col-sm-6">
-                <i className="fa fa-user padding-right-quarter"></i>
-                {circle.get('members').size}/{circle.get('participant_count')}
-                <small className='text-sm text-muted'>: Members</small>
                 </div>
-            </div>
-            <div className="row">
-              <div className="col-sm-6">
-                <i className="fa fa-calendar padding-right-quarter"></i>
-                {format('MMM DD', new Date(circle.get('start_date')))}
-                <small className='text-sm text-muted'>: Starts</small>
+                <div className="text-center margin-top-quarter margin-bottom-quarter">
+                  <span className='margin-right-two'>
+                    <img src={get_avatar_url(circle.get('created_by'))} alt="." className='img-circle w-24'/>
+                  </span>
+                  <Link to={`/users/${circle.getIn(['created_by', 'id'])}`}>
+                    {`${circle.getIn(['created_by', 'firstname'])} ${circle.getIn(['created_by', 'lastname'])}`}
+                  </Link>
+                  <span className={classnames('label margin-left-two', get_trust_score_font_class(circle.getIn(['created_by', 'trust_score'])))}>
+                    {circle.getIn(['created_by', 'trust_score'])}
+                  </span>
+                </div>
               </div>
-              <div className="col-sm-6">
-                <i className="fa fa-refresh padding-right-quarter"></i>
-                {capitalize(circle.get('cycle_period'))}
-                <small className='text-sm text-muted'>: Payouts</small>
-              </div>
+            }
+          </div>
+        </div>
+        <div className="row padding-bottom-one">
+          <div className="col-sm-6">
+            <i className="fa fa-dollar padding-right-quarter"></i>
+            {circle.get('withdrawal_amount')}
+            <small className='text-sm text-muted'>: Payout/cycle</small>
+          </div>
+          <div className="col-sm-6">
+            <i className="fa fa-user padding-right-quarter"></i>
+            {circle.get('members').size}/{circle.get('participant_count')}
+            <small className='text-sm text-muted'>: Members</small>
             </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-6">
+            <i className="fa fa-calendar padding-right-quarter"></i>
+            {format('MMM DD', new Date(circle.get('start_date')))}
+            <small className='text-sm text-muted'>: Starts</small>
+          </div>
+          <div className="col-sm-6">
+            <i className="fa fa-refresh padding-right-quarter"></i>
+            {capitalize(circle.get('cycle_period'))}
+            <small className='text-sm text-muted'>: Payouts</small>
           </div>
         </div>
       </div>
