5a789e0c0eba21223fd9cc72bc4beee5826330cd
quirkles <al.quirk@gmail.com>
2017-02-19 22:21:54 -0500

    test cleanup

--- test/components/create_circle_spec.jsx
+++ test/components/create_circle_spec.jsx
@@ -1,8 +1,8 @@
 /* eslint-env  mocha */
 import React from 'react';
-import {expect, assert} from 'chai';
+import {expect} from 'chai';
 import { shallow } from 'enzyme';
-import {spy} from 'sinon';
+import {stub, spy} from 'sinon';
 
 import {unconnected_create_circle_component as CreateCircle} from '../../src/components/circle/create';
 import CreateCircleForm from '../../src/components/circle/create/create_circle_form';
@@ -35,12 +35,25 @@ describe('CreateCircle Component', () => {
     it('Sets the hover hint on hover', () => {
       const set_hover_hint_to_section = section => () => section;
       const spied_set_hover_hint_to_section = spy(set_hover_hint_to_section);
-      const wrapper = shallow(<CreateCircleForm set_hover_hint_to_section={spied_set_hover_hint_to_section}/>);
-      assert.equal(spied_set_hover_hint_to_section.callCount, 14);
-      wrapper.find('.form-group').first().simulate('mouseenter');
-      wrapper.find('.form-group').first().simulate('mouseleave');
-      assert.equal(spied_set_hover_hint_to_section.returnValues[0](), 'circle_name');
-      assert.equal(spied_set_hover_hint_to_section.returnValues[1](), null);
+      shallow(<CreateCircleForm set_hover_hint_to_section={spied_set_hover_hint_to_section}/>);
+      expect(spied_set_hover_hint_to_section.callCount).to.equal(14);
+      expect(spied_set_hover_hint_to_section.returnValues[0]()).to.equal('circle_name');
+      expect(spied_set_hover_hint_to_section.returnValues[1]()).to.equal(null);
+    });
+
+    it('Fires an edit_circle prop function on changing the start date', () => {
+      const edit_circle = spy();
+      const wrapper = shallow(<CreateCircleForm edit_circle={edit_circle}/>);
+      wrapper.find('input[type="date"]').first().simulate('change', {target: {value: true}});
+      expect(edit_circle.calledOnce).to.be.true;
+    });
+
+    it('Fires an edit_circle_attr prop function on changing the circle name', () => {
+      const edit_circle_attr = attr_name => () => attr_name;
+      const spied_edit_circle_attr = spy(edit_circle_attr);
+      shallow(<CreateCircleForm edit_circle_attr={spied_edit_circle_attr}/>);
+      expect(spied_edit_circle_attr.callCount).to.equal(5);
+      expect(spied_edit_circle_attr.returnValues[0]()).to.equal('circle_name');
     });
   });
 });
